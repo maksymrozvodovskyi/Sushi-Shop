8420ab3675758a664bc57ceeaf40fda75a74ff23
maksym rozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-03-26 17:43:43 +0200

    add functional + && -

--- js/counter-02.js
+++ js/counter-02.js
@@ -1,9 +1,22 @@
 window.addEventListener("click", function (event) {
-  if (event.target.dataset.action === "plus") {
+  let counter;
+
+  if (
+    event.target.dataset.action === "plus" ||
+    event.target.dataset.action === "minus"
+  ) {
     const counterWrapper = event.target.closest(".counter-wrapper");
-    const counter = counterWrapper.querySelector("[data-counter]");
+
+    counter = counterWrapper.querySelector("[data-counter]");
   }
 
-  //   if (event.target.dataset.action === "minus") {
-  //   }
+  if (event.target.dataset.action === "plus") {
+    counter.innerText = ++counter.innerText;
+  }
+
+  if (event.target.dataset.action === "minus") {
+    if (parseInt(counter.innerText) > 1) {
+      counter.innerText = --counter.innerText;
+    }
+  }
 });
